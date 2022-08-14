475dcebd3713068a1761c5e7022f381f60641fa9
BaeSangUn <zyxtkddns@naver.com>
2022-08-14 21:05:29 +0900

    상품페이지 완성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import GPHeader from "./components/GPHeader";
 import GPLayout from "./components/GPLayout";
-import MainPage from "./components/MainPage";
+
 
 function App() {
   return (
