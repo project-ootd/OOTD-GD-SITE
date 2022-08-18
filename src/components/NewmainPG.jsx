e2b889f40c49a49f2550e5327e29c4b960d0f305
nimi2838 <bbongsky11@naver.com>
2022-08-18 23:25:45 +0900

    회원가입,scss 다시 함 백엔드 로그인 미완성

--- src/components/NewmainPG.jsx
+++ src/components/NewmainPG.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/NewmainPG.scss";
-import Login from "./Login";
+import Topbar from "./Topbar";
 
 const NewmainPG = () => {
   return (
@@ -9,127 +9,7 @@ const NewmainPG = () => {
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
       />
-      <header>
-        <h1>
-          <i className="fa-regular fa-eye">&nbsp; OOTD</i>
-        </h1>
-        <nav className="top-bar">
-          <div className="menu-box-1">
-            <ul>
-              <li>
-                <a href="#">MAN</a>
-                <ul>
-                  <li>
-                    <a href="#">SUMMER</a>
-                    <ul>
-                      <li>
-                        <a href="#">TOP</a>
-                      </li>
-                      <li>
-                        <a href="#">BOTTOM</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li>
-                    <a href="#">WINTER</a>
-                    <ul>
-                      <li>
-                        <a href="#">TOP</a>
-                      </li>
-                      <li>
-                        <a href="#">BOTTOM</a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="#">WOMAN</a>
-                <ul>
-                  <li>
-                    <a href="#">SUMMER</a>
-                    <ul>
-                      <li>
-                        <a href="#">TOP</a>
-                      </li>
-                      <li>
-                        <a href="#">BOTTOM</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li>
-                    <a href="#">WINTER</a>
-                    <ul>
-                      <li>
-                        <a href="#">TOP</a>
-                      </li>
-                      <li>
-                        <a href="#">BOTTOM</a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="#">KIDS</a>
-                <ul>
-                  <li>
-                    <a href="#">SUMMER</a>
-                    <ul>
-                      <li>
-                        <a href="#">TOP</a>
-                      </li>
-                      <li>
-                        <a href="#">BOTTOM</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li>
-                    <a href="#">WINTER</a>
-                    <ul>
-                      <li>
-                        <a href="#">TOP</a>
-                      </li>
-                      <li>
-                        <a href="#">BOTTOM</a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="#">ACCESSORY</a>
-                <ul>
-                  <li>
-                    <a href="#">RING</a>
-                    <ul>
-                      <li>
-                        <a href="#">GOLD</a>
-                      </li>
-                      <li>
-                        <a href="#">DIAMOND</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li>
-                    <a href="#">NECKLACE</a>
-                    <ul>
-                      <li>
-                        <a href="#">GOLD</a>
-                      </li>
-                      <li>
-                        <a href="#">DIAMOND</a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <Login />
-      </header>
-
+      <Topbar />
       <section>
         <div className="img">
           <div className="content">
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -4,131 +4,124 @@ import { Link } from "react-router-dom";
 import Login from "./Login";
 const Topbar = () => {
   return (
-    <div>
-      <link
-        rel="stylesheet"
-        Link
-        to="https://cdLinkjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
-      />
-      <header>
-        <h1>
-          <i className="fa-regular fa-eye">&nbsp; OOTD</i>
-        </h1>
-        <nav className="top-bar">
-          <div className="menu-box-1">
-            <ul>
-              <li>
-                <Link to="#">MAN</Link>
-                <ul>
-                  <li>
-                    <Link to="#">SUMMER</Link>
-                    <ul>
-                      <li>Link </li>
-                      <li>
-                        <Link to="#">BOTTOM</Link>
-                      </li>
-                    </ul>
-                  </li>
-                  <li>
-                    <Link to="#">WINTER</Link>
-                    <ul>
-                      <li>
-                        <Link to="#">TOP</Link>
-                      </li>
-                      <li>
-                        <Link to="#">BOTTOM</Link>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <Link to="WGDP">WOMAN</Link>
-                <ul>
-                  <li>
-                    <Link to="#">SUMMER</Link>
-                    <ul>
-                      <li>
-                        <Link to="#">TOP</Link>
-                      </li>
-                      <li>
-                        <Link to="#">BOTTOM</Link>
-                      </li>
-                    </ul>
-                  </li>
-                  <li>
-                    <Link to="#">WINTER</Link>
-                    <ul>
-                      <li>
-                        <Link to="#">TOP</Link>
-                      </li>
-                      <li>
-                        <Link to="#">BOTTOM</Link>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <Link to="#">KIDS</Link>
-                <ul>
-                  <li>
-                    <Link to="#">SUMMER</Link>
-                    <ul>
-                      <li>
-                        <Link to="#">TOP</Link>
-                      </li>
-                      <li>
-                        <Link to="#">BOTTOM</Link>
-                      </li>
-                    </ul>
-                  </li>
-                  <li>
-                    <Link to="#">WINTER</Link>
-                    <ul>
-                      <li>
-                        <Link to="#">TOP</Link>
-                      </li>
-                      <li>
-                        <Link to="#">BOTTOM</Link>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <Link to="#">ACCESSOLY</Link>
-                <ul>
-                  <li>
-                    <Link to="#">RING</Link>
-                    <ul>
-                      <li>
-                        <Link to="#">GOLD</Link>
-                      </li>
-                      <li>
-                        <Link to="#">DIAMOND/</Link>
-                      </li>
-                    </ul>
-                  </li>
-                  <li>
-                    <Link to="#">NECKLACE</Link>
-                    <ul>
-                      <li>
-                        <Link to="#">GOLD</Link>
-                      </li>
-                      <li>
-                        <Link to="#">DIAMOND</Link>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <Login />
-      </header>
-    </div>
+    <header>
+      <h1>
+        <i className="fa-regular fa-eye">&nbsp; OOTD</i>
+      </h1>
+      <nav className="top-bar">
+        <div className="menu-box-1">
+          <ul>
+            <li>
+              <Link to="#">MAN</Link>
+              <ul>
+                <li>
+                  <Link to="#">SUMMER</Link>
+                  <ul>
+                    <li>Link </li>
+                    <li>
+                      <Link to="#">BOTTOM</Link>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <Link to="#">WINTER</Link>
+                  <ul>
+                    <li>
+                      <Link to="#">TOP</Link>
+                    </li>
+                    <li>
+                      <Link to="#">BOTTOM</Link>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link to="/WGDP">WOMAN</Link>
+              <ul>
+                <li>
+                  <Link to="#">SUMMER</Link>
+                  <ul>
+                    <li>
+                      <Link to="#">TOP</Link>
+                    </li>
+                    <li>
+                      <Link to="#">BOTTOM</Link>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <Link to="#">WINTER</Link>
+                  <ul>
+                    <li>
+                      <Link to="#">TOP</Link>
+                    </li>
+                    <li>
+                      <Link to="#">BOTTOM</Link>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link to="#">KIDS</Link>
+              <ul>
+                <li>
+                  <Link to="#">SUMMER</Link>
+                  <ul>
+                    <li>
+                      <Link to="#">TOP</Link>
+                    </li>
+                    <li>
+                      <Link to="#">BOTTOM</Link>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <Link to="#">WINTER</Link>
+                  <ul>
+                    <li>
+                      <Link to="#">TOP</Link>
+                    </li>
+                    <li>
+                      <Link to="#">BOTTOM</Link>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link to="#">ACCESSOLY</Link>
+              <ul>
+                <li>
+                  <Link to="#">RING</Link>
+                  <ul>
+                    <li>
+                      <Link to="#">GOLD</Link>
+                    </li>
+                    <li>
+                      <Link to="#">DIAMOND/</Link>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <Link to="#">NECKLACE</Link>
+                  <ul>
+                    <li>
+                      <Link to="#">GOLD</Link>
+                    </li>
+                    <li>
+                      <Link to="#">DIAMOND</Link>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Login />
+    </header>
   );
 };
 
