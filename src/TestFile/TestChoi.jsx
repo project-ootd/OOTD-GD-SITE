6b61452d26dd5dd165baecc09b602ef4d74e2b99
ahcoo <mynameisjins@gmail.com>
2022-08-19 21:43:10 +0900

    completed to branch set

--- src/TestFile/TestChoi.jsx
+++ src/TestFile/TestChoi.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Table, Checkbox, Mask, Badge, Button } from "react-daisyui";
 import "../styles/Test/TestChoi.scss";
+import Buy from "../btn/Buy";
 
 import { Link } from "react-router-dom";
 
@@ -56,20 +57,59 @@ const TestChoi = (args) => {
                 {/* <Badge color="ghost" size="sm"></Badge> */}
               </div>
               <div>53,000</div>
-              <Button color="ghost" size="xs" style={{ color: "red" }}>
+              <Button color="ghost" size="md" style={{ color: "red" }}>
                 삭제
               </Button>
             </Table.Row>
           </Table.Body>
 
+          <Table.Body>
+            <Table.Row>
+              <div></div>
+
+              <div className="flex items-center space-x-3 truncate">
+                <Mask />
+                <div>
+                  <div className="font-bold"></div>
+                  <div className="text-sm opacity-50"></div>
+                </div>
+              </div>
+              <div style={{ textAlign: "center", fontWeight: "bold" }}>
+                총 수량<br></br>
+                <Badge color="ghost" size="lg">
+                  1
+                </Badge>
+              </div>
+              <div style={{ textAlign: "center", fontWeight: "bold" }}>
+                총 가격<br></br>
+                <Badge
+                  color="ghost"
+                  size="lg"
+                  style={{ fontWeight: "bold", color: "red" }}
+                >
+                  53,000
+                </Badge>
+              </div>
+              <Button color="ghost" size="xs" style={{ color: "red" }}></Button>
+            </Table.Row>
+          </Table.Body>
+
           <Table.Footer>
             <span>&nbsp;</span>
-            <span></span>
-            <span>총 수량</span>
-            <span>총 가격</span>
             <span>&nbsp;</span>
+            <span></span>
+            <Button {...args}>돌아가기</Button>
+            <Button {...args} color="secondary">
+              결제하기
+            </Button>
           </Table.Footer>
         </Table>
+      </div>
+      <div class="mockup-window border bg-base-300">
+        <div class="flex justify-center px-4 py-16 bg-base-200">
+          <Buy />
+          Hello!
+        </div>
       </div>
     </>
   );
