0397f18424203408cddf455a334bd493c1c3104b
ansangyu <moto1515@gmail.com>
2022-08-24 18:10:05 +0900

    링페이지 수정

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "tailwindcss/tailwind.css";
 // import Join from "./Page/Join";
 // import axios from "axios";
 
-<<<<<<< Updated upstream
 import Topbar from "./components/Topbar";
 import MainPG from "./components/MainPG";
 import Footer from "./components/Footer";
@@ -13,7 +12,7 @@ import MGDP from "./Page/MGDP";
 import Payment from "./components/Payment";
 import Popup from "./components/Pop-up";
 import Sub_detail_man from "./sub_detail/Sub_detail_man";
-=======
+
 // import Topbar from "./components/Topbar";
 // import MainPG from "./components/MainPG";
 // import Footer from "./components/Footer";
@@ -23,19 +22,17 @@ import Sub_detail_man from "./sub_detail/Sub_detail_man";
 // import Payment from "./components/Payment";
 // import Popup from "./components/Pop-up";
 import Carousel from "./components/Carousel";
->>>>>>> Stashed changes
 
 function App() {
   return (
     <div>
-<<<<<<< Updated upstream
       <Topbar />
       {/* <Sub_detail /> */}
 
       <MainPG />
 
       <Footer />
-=======
+
       {/* <Topbar />
       <MainPG /> */}
 
@@ -46,7 +43,6 @@ function App() {
       <Footer /> */}
 
       <Carousel />
->>>>>>> Stashed changes
     </div>
   );
 }
