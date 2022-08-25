4388e777b333b62bbafbf6666ac59c63f98d66e0
BaeSangUn <zyxtkddns@naver.com>
2022-08-26 00:14:47 +0900

    가격 수정 및 안쓰는 코드 정리

--- src/components/NewmainPG.jsx
+++ src/components/NewmainPG.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/NewmainPG.scss";
 import Topbar from "./Topbar";
 
@@ -7,7 +8,7 @@ const NewmainPG = () => {
     <div>
       <link
         rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+        to="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
       />
       <Topbar />
       <section>
@@ -24,24 +25,26 @@ const NewmainPG = () => {
         <div className="announce">
           <ul className="announce-top">
             <li>
-              <a href="#">ANNOUNCE</a>
+              <Link to="#">ANNOUNCE</Link>
             </li>
           </ul>
           <ul className="announce-list">
             <li>
-              <a href="#">5. 침수로 인한 배송지연 안내</a>
+              <Link to="#">5. 침수로 인한 배송지연 안내</Link>
             </li>
             <li>
-              <a href="#">4. 상담센터 증설 안내</a>
+              <Link to="#">4. 상담센터 증설 안내</Link>
             </li>
             <li>
-              <a href="#">3. 서비스 확장에 따른 임시휴무 안내</a>
+              <Link to="#">3. 서비스 확장에 따른 임시휴무 안내</Link>
             </li>
             <li>
-              <a href="#">2. 입고 지연 안내</a>
+              <Link to="#">2. 입고 지연 안내</Link>
             </li>
             <li>
-              <a href="#">1. 인터넷 쇼핑몰 입점 안내(쿠팡, G-market, Amazon)</a>
+              <Link to="#">
+                1. 인터넷 쇼핑몰 입점 안내(쿠팡, G-market, Amazon)
+              </Link>
             </li>
           </ul>
         </div>
@@ -49,24 +52,24 @@ const NewmainPG = () => {
         <div className="event">
           <ul className="event-top">
             <li>
-              <a href="#">EVENT</a>
+              <Link to="#">EVENT</Link>
             </li>
           </ul>
           <ul className="event-list">
             <li>
-              <a href="#">5. 수영복 특가할인행사(8/1~8/15)</a>
+              <Link to="#">5. 수영복 특가할인행사(8/1~8/15)</Link>
             </li>
             <li>
-              <a href="#">4. 여름맞이 할인행사(7/1~7/14)</a>
+              <Link to="#">4. 여름맞이 할인행사(7/1~7/14)</Link>
             </li>
             <li>
-              <a href="#">3. 여름방학맞이 키즈상품 할인행사(6/1~6/20)</a>
+              <Link to="#">3. 여름방학맞이 키즈상품 할인행사(6/1~6/20)</Link>
             </li>
             <li>
-              <a href="#">2. 가입자 100만 돌파기념 할인행사(5/20~5/31)</a>
+              <Link to="#">2. 가입자 100만 돌파기념 할인행사(5/20~5/31)</Link>
             </li>
             <li>
-              <a href="#">1. 새분기 새학기맞이 행사(3/2~3/15)</a>
+              <Link to="#">1. 새분기 새학기맞이 행사(3/2~3/15)</Link>
             </li>
           </ul>
         </div>
