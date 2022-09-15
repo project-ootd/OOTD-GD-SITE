6da4a1a33209d29be77a089097689fbd9446bb57
BaeSangUn <zyxtkddns@naver.com>
2022-09-15 14:29:35 +0900

    팝업,서치 완성

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Topbar from "./components/Topbar";
 import MainPG from "./components/MainPG";
 import Footer from "./components/Footer";
 import Popup from "./components/Pop-up";
-import NoticeBoard from "./board/NoticeBoard";
-import NoticePost from "./board/NoticePost";
 
 // WGDP 페이지 prdno 에러
 // Woman 페이지 state 배열로 못 넣는지 물어보기
@@ -41,6 +39,7 @@ function App() {
   return (
     <div>
       <Topbar />
+      <Popup />
       <MainPG />
       <Footer />
     </div>
