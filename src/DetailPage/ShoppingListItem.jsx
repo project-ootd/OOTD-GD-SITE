9dd9c69f34b7b5977924d0c8dc95994ecb28ca4f
BaeSangUn <zyxtkddns@naver.com>
2022-09-19 18:42:53 +0900

    장바구니 수정 중

--- src/DetailPage/ShoppingListItem.jsx
+++ src/DetailPage/ShoppingListItem.jsx
@@ -1,39 +1,71 @@
 import React from "react";
 
 const ShoppingListItem = ({ CartArr }) => {
+  console.log(CartArr);
   return (
     <div className="Shopping-list-item">
       <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
+        <td>
+          <div></div>
+        </td>
         <td>
           <div className="flex items-center space-x-3">
             <div className="avatar">
               <div className="mask mask-squircle w-12 h-12">
-                <img
-                  src="/tailwind-css-component-profile-2@56w.png"
-                  alt="Avatar Tailwind CSS Component"
-                />
+                <img src={CartArr.prdImg} alt="Avatar Tailwind CSS Component" />
               </div>
             </div>
             <div>
               <div className="font-bold">{CartArr.prdName}</div>
-              <div className="text-sm opacity-50">{CartArr.prdPrice}</div>
+              <div className="text-sm opacity-50">{CartArr.prdEname}</div>
             </div>
           </div>
         </td>
-        <td>
-          {CartArr.prdName}
+        <td></td>
+        <td
+          style={{
+            display: "flex",
+          }}
+        >
+          <div
+            className="minus"
+            style={{
+              display: "flex",
+              width: "30px",
+              height: "48px",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          ></div>
+          <div
+            style={{
+              display: "flex",
+              width: "30px",
+              height: "48px",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          ></div>
+          <div
+            className="plus"
+            style={{
+              display: "flex",
+              width: "30px",
+              height: "48px",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          ></div>
           <br />
-          <span className="badge badge-ghost badge-sm">{CartArr.prdName}</span>
+          {/* <span className="badge badge-ghost badge-sm"></span> */}
         </td>
-        <td>{CartArr.prdName}</td>
-        <th>
-          <button className="btn btn-ghost btn-xs">{CartArr.prdName}</button>
-        </th>
+        <td>{CartArr.prdPrice}</td>
+        {/* <td>{list.prdPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td> */}
+        <td>
+          <button className="btn btn-ghost btn-mg" style={{ color: "red" }}>
+            삭제
+          </button>
+        </td>
       </tr>
     </div>
   );
